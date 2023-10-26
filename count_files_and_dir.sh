7f220759cffc0a54aa26cd4b323fcbfb8de0096e
Ubuntu <ubuntu@ip-172-31-46-132.ap-south-1.compute.internal>
2023-10-26 01:29:45 +0000

    Script to count number of files and directries

---
+++ count_files_and_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter the directory path"
+read p
+
+#check the path is correct or not
+if [ -z "$p" ]; then
+	echo "Please provide the directory name" 
+else
+	echo "Directory name is $p"
+fi
+
+#To get the number of files
+files_count=$(find "$p" -type f | wc -l)
+
+#To get the number of directories
+directories_count=$(find "$p" -type  d | wc -l)
+
+#Print the results
+echo "Number of files is $files_count and Number of directories is $directories_count"
